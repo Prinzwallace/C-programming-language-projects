aa3b583147a6d04e6746e84b8c2f1d61dedbdfc9
Prinzwallace <prinzk442@gmail.com>
2022-10-11 17:13:07 -0700

    more projects

--- arrays/1-array.c
+++ arrays/1-array.c
@@ -5,13 +5,13 @@ int main(){
 int a[5], b[5], c[5], i;
 
 for(i = 0; i < 5; i++){
-scanf("%d %d", a[i], b[i]);
+scanf("%d %d", &a[i], &b[i]);
 printf("input next set\n");
 }
 for(i = 0; i < 5; i++){
 
 c[i]=a[i]+b[i];
-printf("sum of arrays = \n", c[i]);
+printf("sum of arrays = %d\n", c[i]);
 }
 return (0);
 }
